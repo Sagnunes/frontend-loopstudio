7fbdfe0f10ed1852ef545461eaf5b5e489328320
Sérgio Nunes <sagnunes@gmail.com>
2024-02-28 14:26:26 +0000

    finish mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        backgroundImage: {
+            "hero-mobile": "url(../images/mobile/image-hero.jpg)",
+            "hero-desktop": "url(../images/desktop/image-hero.jpg)",
+        },
+        colors: {
+            white: "hsl(0, 0%, 100%)",
+            black: "hsl(0, 0%, 0%)",
+            gray: {
+                dark: "hsl(0, 0%, 55%)",
+                "very-dark": "hsl(0, 0%, 41%)"
+            }
+        },
+        fontFamily: {
+            "alata": ['Alata', "sans-serif"],
+            "josefin-sans": ["Josefin Sans", "sans-serif"]
+        }
+    },
+    plugins: [],
 }
